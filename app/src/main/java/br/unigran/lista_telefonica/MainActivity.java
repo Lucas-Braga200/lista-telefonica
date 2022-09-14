bee08ff202443c23e3eabc09d5b96931eb9905da
Lucas Braga <802.266@alunos.unigran.br>
2022-09-13 20:23:48 -0400

    Criação do projeto

---
+++ app/src/main/java/br/unigran/lista_telefonica/MainActivity.java
@@ -0,0 +1,14 @@
+package br.unigran.lista_telefonica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
